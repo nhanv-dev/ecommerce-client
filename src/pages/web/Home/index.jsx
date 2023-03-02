cb823925a43e728b7e2b469089b2765f342b04fa
Trần Thanh Nhân <66198815+nhanv-dev@users.noreply.github.com>
2023-03-02 16:47:42 +0700

    update

--- src/pages/web/Home/index.jsx
+++ src/pages/web/Home/index.jsx
@@ -1,11 +1,11 @@
 import React, {useState, useEffect, useContext} from 'react';
-import {SocketContext} from "../../../service/socket";
 import Helmet from "../../../components/web/Helmet";
 import {UserLayout} from "../../../components/common/Layouts";
 import "./style.scss";
 import {useSelector} from "react-redux";
 import ProductCard from "../../../components/web/ProductCard";
 import {publicRequest} from "../../../utils/requestMethods";
+import {formatLongDate} from "../../../utils/format";
 
 
 function Home() {
@@ -14,6 +14,7 @@ function Home() {
     const [products, setProducts] = useState([])
     useEffect(() => {
         publicRequest.get(`/products/all`).then(res => {
+            console.log(formatLongDate(res.data.products[res.data.products.length - 1].updatedAt))
             setProducts(res.data.products)
         })
     }, [])
@@ -25,7 +26,7 @@ function Home() {
                     <div className="bg-white mb-5 rounded-[5px]">
                         <div className="border-b flex border-border">
                             <div className="ml-6 p-2 text-primary border-b-4 border-primary font-semibold">
-                               Sản phẩm mới nhất
+                                Sản phẩm mới nhất
                             </div>
                         </div>
                     </div>
